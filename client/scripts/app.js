80feac7aa37abc1220350aa29b6e42892694ca74
rroque98 <rroque98@gmail.com>
2018-06-22 16:04:59 -0700

    Sends, fetches, clears, adds messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,68 @@
-// YOUR CODE HERE:
+
+var app = {
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox',
+
+};
+
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('message received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.renderMessage = function(message) {
+  $('#chats').append(`<div class="message"><div class="username">${message.username}:</div> ${message.text} </div>`);
+};
+
+app.renderRoom = function(room) {
+  $('#roomSelect').append(`<option>${room}</option>`);
+};
+
+app.init = function() {
+  $('.username').on('click', function() {
+    console.log('hello'); 
+  });
+
+};
+
+
+
+var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
