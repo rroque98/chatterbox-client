a44c85e7ec41314d6808f60391e9b07a4b3eec37
rroque98 <rroque98@gmail.com>
2018-06-22 17:30:05 -0700

    Passes all tests through events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,6 @@
 
 var app = {
-  server: 'http://parse.sfm8.hackreactor.com/chatterbox',
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
 
 };
 
@@ -25,7 +25,7 @@ app.fetch = function() {
 
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfm8.hackreactor.com/chatterbox',
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
     data: JSON.stringify(message),
     contentType: 'application/json',
@@ -44,20 +44,41 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  $('#chats').append(`<div class="message"><div class="username">${message.username}:</div> ${message.text} </div>`);
+  $('#chats').append(`<div class="message"><div class="username" 
+data-username="${message.username}">${message.username}:</div> ${message.text} </div>`);
 };
 
 app.renderRoom = function(room) {
   $('#roomSelect').append(`<option>${room}</option>`);
 };
 
-app.init = function() {
-  $('.username').on('click', function() {
-    console.log('hello'); 
-  });
 
+
+app.handleUsernameClick = function(event) {
+  $('.friendlist').append(`<div> ${$('.username').data()} </div>`);
 };
 
+app.handleSubmit =  function() {
+  message.username = "username";
+  message.roomname = $('#roomSelect');
+  message.text = $('#message').val();
+  app.send(message);
+}
+
+
+app.init = function() {
+  $('.username').on('click', app.handleUsernameClick());
+  $('button').on('click', app.handleSubmit());
+};
+
+// $(document).ready(function() {
+//   $('.username').on('click', function() {
+//     $('.username').append('<p>friend added!</p>');
+//   //   //console.log('hello');
+//   });
+
+//   });
+// });
 
 
 var message = {
